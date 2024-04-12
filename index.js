a3620857771b10d412c5e469f5216aa78ed5e7d5
Teo Jiung Bin <tjiungbin@gmail.com>
2024-04-13 01:45:41 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function toggleMenu (){
+    var menu = document.getElementById("menu");
+
+    menu.classList.contains("hidden") ? menu.classList.remove("hidden") : menu.classList.add("hidden");
+}
